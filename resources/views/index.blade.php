3be3b32c03de3504018ab6bdaca67ff1c61c773c
vagrant <vagrant@homestead>
2015-11-28 04:25:40 +0000

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 	@foreach($posts as $post)
     <div class="entry">
-    <a class="entry-thumb" href="{{env('UPLOAD_PATH').$post->file}}" title="下载 {{$post->title}}" target="_blank">
+    <a class="entry-thumb" href="{{env('UPLOAD_PATH').$post->file}}" title="预览 {{$post->title}}" target="_blank">
       <p><img src="{{env('UPLOAD_PATH').$post->thumb}}"></p>
 
       <span class="icon icon-forward entry-icon"></span>
@@ -11,7 +11,7 @@
       <h2 class="entry-title">
         <a href="{{env('UPLOAD_PATH').$post->file}}">{{$post->title}}</a>
       </h2>
-      <p class="entry-date">点击下载</p>
+      <p class="entry-date">右键点击标题另存为下载</p>
     </div>
   </div>
   @endforeach
